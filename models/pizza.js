b3d68d8ff0f0d0309056b3f86609c7aa68c28cd0
Marina <dubovik.m14@gmail.com>
2017-12-17 01:26:47 +0200

    add pizza's model

--- models/pizza.js
+++ models/pizza.js
@@ -1,5 +1,21 @@
 module.exports = (Sequelize, sequelize) => {
     return sequelize.define('pizzas', {
-      // TODO: описание полей
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      name: {
+          type: Sequelize.STRING,
+          allowNull: false
+      },
+      description: {
+          type: Sequelize.STRING,
+          allowNull: false
+      },
+      calories: {
+          type: Sequelize.DOUBLE,
+          allowNull: false
+      }
     });
   };
